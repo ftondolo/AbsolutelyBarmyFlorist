734708c364aa9ec4458c56c45e5ff6a832086013
Federico Tondolo <ftondolo@uchicago.edu>
2019-06-21 17:06:12 -0400

    Added Comments

--- AbsolutelyBarmyFlorist.py
+++ AbsolutelyBarmyFlorist.py
@@ -8,16 +8,18 @@ import matplotlib.pyplot as plt
 def main():
     # Creating OUTPUT directory
     os.mkdir('OUTPUT')
-    # Loops for every file inside current dir
+    # Loop for every file inside current dir
     for filename in os.listdir('.'):
         # If file is a video file
         if (filename.endswith(".abf")):
+            # Variable Initialisation
             abf = pyabf.ABF(filename)
             abf.setSweep(0)
             previous_status=abf.sweepC[0]
             partial_sum=0
             counter=0
             channel=0
+            # Plotting Bonanza
             if (len(abf.tagComments)> 0):
                 plot_comments(abf)
             if (abf.channelCount>1):
@@ -25,26 +27,33 @@ def main():
             if (abf.sweepCount>0):
                 plot_sweeps(abf)
             plot_epoch(abf)
+            # Loop for every sweep
             for y in range(abf.sweepCount):
                 abf.setSweep(y)
+                # Printing Epochs
                 print('SWEEP #%d' % y)
                 for i, p1 in enumerate(abf.sweepEpochs.p1s):
                     epochLevel = abf.sweepEpochs.levels[i]
                     epochType = abf.sweepEpochs.types[i]
                     print(f"Epoch index {i}: At point {p1} there is a {epochType} to level {epochLevel}")
+                # Loop for every channel
                 while channel < abf.channelCount:
                     channel+=1
                     x=channel-1
+                    # Creation of CSV file
                     with open('./OUTPUT/%s-sweep%d-ch%d.csv' % (filename[:-4], y, channel), 'w') as csvfile:
                         filewriter = csv.writer(csvfile, delimiter=',',
                                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
                         filewriter.writerow(['Time (s)', 'Command (DAC)', 'Reading (ADC)', 'Average ADC per DAC'])
+                        # Loop for entire recorded time
                         while (x <len(abf.sweepY)):
+                            # If DAC has changed
                             if ((x!=len(abf.sweepY)-1) | (previous_status==abf.sweepC[x])):
                                 partial_sum+=abf.sweepY[x]
                                 counter+=1
                                 filewriter.writerow([abf.sweepX[x], abf.sweepC[x], abf.sweepY[x], 'TBD'])
                                 x+=abf.channelCount
+                            # If DAC has not changed
                             else:
                                 partial_sum/=counter
                                 previous_status=abf.sweepC[x]
@@ -55,26 +64,33 @@ def main():
         else:
             continue
 
+# Plot creation following recorded epochs
 def plot_epoch(abfinput):
+    # Window
     fig = plt.figure(figsize=(8, 5))
+    # Graph 1
     ax1 = fig.add_subplot(211)
     ax1.plot(abfinput.sweepY, color='b')
     ax1.set_ylabel("ADC (measurement)")
     ax1.set_xlabel("Sweep point (index)")
 
+    # Graph 2
     ax2 = fig.add_subplot(212)
     ax2.plot(abfinput.sweepC, color='r')
     ax2.set_ylabel("DAC (command)")
     ax2.set_xlabel("Sweep point (index)")
 
+    # Loop through epochs
     for p1 in abfinput.sweepEpochs.p1s:
         ax1.axvline(p1, color='k', ls='--', alpha=.5)
         ax2.axvline(p1, color='k', ls='--', alpha=.5)
 
+    # Show
     plt.tight_layout()
     plt.show()
 
 def plot_sweeps(abfinput):
+    # Window
     plt.figure(figsize=(8, 5))
     plt.title("Sweeps")
     plt.ylabel(abfinput.sweepLabelY)
@@ -82,26 +98,29 @@ def plot_sweeps(abfinput):
     for i in range(abfinput.sweepCount):
         abfinput.setSweep(i)
         plt.plot(abfinput.sweepX, abfinput.sweepY, alpha=.5, label="sweep %d"%(i+1))
+    # Show
     plt.legend()
     plt.show()
 
 def plot_channels(abfinput):
+    # Window
     fig = plt.figure(figsize=(8, 5))
-    # plot the first channel
+    # graph the first channel
     abfinput.setSweep(sweepNumber=0, channel=0)
     plt.plot(abfinput.sweepX, abfinput.sweepY, label="Channel 1")
-    # plot the second channel
+    # Graph the second channel
     abfinput.setSweep(sweepNumber=0, channel=1)
     plt.plot(abfinput.sweepX, abfinput.sweepY, label="Channel 2")
-    # decorate the plot
+    # Decorating the graph
     plt.ylabel(abfinput.sweepLabelY)
     plt.xlabel(abfinput.sweepLabelX)
     plt.axis([25, 45, -70, 50])
+    # Show
     plt.legend()
     plt.show()
 
 def plot_comments(abfinput):
-    # create a plot with time on the horizontal axis
+    # Window
     plt.figure(figsize=(8, 5))
     for sweep in abfinput.sweepList:
         abfinput.setSweep(sweep, absoluteTime=True) # <-- relates to sweepX
@@ -115,6 +134,7 @@ def plot_comments(abfinput):
         comment = abfinput.tagComments[i]
         color = "C%d"%(i+1)
         plt.axvline(posX, label=comment, color=color, ls='--')
+    # Show
     plt.legend()
     plt.title("ABF File with Tags")
     plt.show()
--- SimplisticFlorist.py
+++ SimplisticFlorist.py
@@ -8,10 +8,11 @@ import matplotlib.pyplot as plt
 def main():
     # Creating OUTPUT directory
     os.mkdir('OUTPUT')
-    # Loops for every file inside current dir
+    # Loop for every file inside current dir
     for filename in os.listdir('.'):
         # If file is a video file
         if (filename.endswith(".abf")):
+            # Variable Initialisation
             abf = pyabf.ABF(filename)
             abf.setSweep(0)
             previous_status=abf.sweepC[0]
@@ -19,21 +20,27 @@ def main():
             counter=0
             channel=0
             x=0
+            # Loop for every sweep
             for y in range(abf.sweepCount):
                 abf.setSweep(y)
+                # Loop for every channel
                 while channel < abf.channelCount:
                     channel+=1
                     x=channel-1
+                    # Creation of CSV file
                     with open('./OUTPUT/%s-sweep%d-ch%d.csv' % (filename[:-4], y, channel), 'w') as csvfile:
                         filewriter = csv.writer(csvfile, delimiter=',',
                                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
                         filewriter.writerow(['Time (s)', 'Command (DAC)', 'Reading (ADC)', 'Average ADC per DAC'])
+                        # Loop for entire recorded time
                         while (x <len(abf.sweepY)):
+                            # If DAC has changed
                             if (previous_status==abf.sweepC[x]):
                                 partial_sum+=abf.sweepY[x]
                                 counter+=1
                                 filewriter.writerow([abf.sweepX[x], abf.sweepC[x], abf.sweepY[x], 'TBD'])
                                 x+=abf.channelCount
+                                # If DAC has not changed
                             else:
                                 partial_sum/=counter
                                 previous_status=abf.sweepC[x]
